0be2f006f2931bce5a62bd319b81a3374fc03833
Poliana Santos <99842806+PollySantos@users.noreply.github.com>
2025-08-06 14:42:47 +0100

    Ajustando espaçamento

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-let nomes = []; // Lista de nomes inseridos
+let nomes = []; 
 
-// Função para adicionar nome à lista
 function adicionarNome() {
     const input = document.getElementById("nome");
     const nome = input.value.trim();
@@ -20,7 +19,6 @@ function adicionarNome() {
     mostrarNomes();
 }
 
-// Função para exibir os nomes na lista
 function mostrarNomes() {
     const lista = document.getElementById("listaNomes");
     lista.innerHTML = "";
@@ -33,29 +31,25 @@ function mostrarNomes() {
     });
 }
 
-// Função para sortear apenas um nome da lista
 function sortearAmigo() {
     if (nomes.length === 0) {
         alert("A lista está vazia. Adicione pelo menos um nome.");
         return;
     }
 
-    // Gera um índice aleatório baseado no tamanho da lista
     const indiceSorteado = Math.floor(Math.random() * nomes.length);
     
-    // Pega o nome correspondente ao índice sorteado
     const nomeSorteado = nomes[indiceSorteado];
 
-    // Exibe o resultado do sorteio
     document.getElementById("resultado").innerHTML = `
         <h3>Nome sorteado:</h3>
         <p>${nomeSorteado}</p>
     `;
 }
 
-// Função para reiniciar o sistema
 function reiniciar() {
     nomes = [];
     document.getElementById("listaNomes").innerHTML = "";
     document.getElementById("resultado").textContent = "";
+
 }
